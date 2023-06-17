5f67f1d59324d498c10e2d5d28a0d430993974ef
bk_viper <90303435+buKaushalya@users.noreply.github.com>
2023-06-17 12:54:55 +0530

    end of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -10,31 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
-
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
-  AddJwtBearer(options => {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-      ValidateIssuerSigningKey = true,
-      IssuerSigningKey = new SymmetricSecurityKey(Encoding
-          .UTF8.GetBytes(builder.Configuration["TokenKey"])),
-      ValidateIssuer = false,
-      ValidateAudience = false
-    };
-  });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
 
@@ -42,5 +27,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+  var context = services.GetRequiredService<DataContext>();
+  await context.Database.MigrateAsync();
+  await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+  var logger = services.GetService<ILogger<Program>>();
+  logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
